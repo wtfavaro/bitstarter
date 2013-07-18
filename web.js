5b7568652ba0d2c7e8010f52f244f9a88fea2545
William Favaro <wtfavaro@hotmail.com>
2013-07-18 01:42:29 +0000

    Another sync attempt..

--- web.js
+++ web.js
@@ -1,15 +1,6 @@
-var express = require('express');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
     fs.readFileSync('index.html' function (err, data) {
         if (err) throw err;
         response.send(data);
     });
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
