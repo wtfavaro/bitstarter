06fde43775e078c8474e6127eddf3491443351a7
William Favaro <wtfavaro@hotmail.com>
2013-07-18 01:52:32 +0000

    Edit to readFileSync.

--- web.js
+++ web.js
@@ -1,6 +1,14 @@
+var express = require('express');
 
-    fs.readFileSync('index.html' function (err, data) {
-        if (err) throw err;
-        response.send(data);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var k = fs.readFileSync('index.html', 'utf8');
+        response.send(k);
     });
+});
 
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
