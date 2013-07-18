2fdb33632847194a089f40096941624e0c49a39c
William Favaro <wtfavaro@hotmail.com>
2013-07-18 01:31:02 +0000

    Another sync attempt.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     fs.readFileSync('index.html' function (err, data) {
         if (err) throw err;
-        console.log(data);
+        response.send(data);
     });
 });
 
